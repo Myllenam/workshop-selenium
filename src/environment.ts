b7ac002806e15f26a92155ea2c719a141e41f547
Myllena Miranda <myllena.miranda@klab.com.br>
2024-09-05 22:04:19 -0300

    Ajuste de config

--- src/environment.ts
+++ src/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
     project_url: 'https://www.airbnb.com.br/',
     delay: false,
     delayTimeout: 2 * 1000,
-    timeout: 60 * 1000,
+    timeout: 60 * 5000,
   }
   
