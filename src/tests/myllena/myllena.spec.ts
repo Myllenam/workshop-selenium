9b19f0208062a5503872bd8f46636367894b7907
Myllena Miranda <myllena.miranda@klab.com.br>
2024-09-08 14:04:40 -0300

    TM-Myllena testes finalizados

--- src/tests/myllena/myllena.spec.ts
+++ src/tests/myllena/myllena.spec.ts
@@ -1,28 +1,26 @@
-import { describe, beforeEach, it, beforeAll, afterAll } from 'vitest';
+import { describe, beforeEach, it, beforeAll, afterAll } from "vitest";
 
-import {MyllenaFlow} from "../../flows/myllena.flow"
-import {SeleniumDriver} from "../../drivers/selenium-driver"
+import { MyllenaFlow } from "../../flows/myllena.flow";
+import { SeleniumDriver } from "../../drivers/selenium-driver";
 
-
-describe('Myllena', () => {
+describe("Myllena", () => {
   let myllenaFlow: MyllenaFlow;
 
   beforeAll(async () => {
     myllenaFlow = new MyllenaFlow(new SeleniumDriver());
   });
 
-  afterAll(async () => {
-    await myllenaFlow.closeDriver();
-  });
+  afterAll(async () => {});
 
   beforeEach(async () => {
     await myllenaFlow.goToInitialPage();
   });
 
-  // it('deve verficar o fluxo de adicionar itens ao carrinho', async () => {
-  //   await myllenaFlow.cartFlow();
-  // });
-  it('deve verficar o fluxo de adicionar itens ao carrinho e de modificar suas quantidades', async () => {
+  it("deve verficar o fluxo de adicionar itens ao carrinho", async () => {
+    await myllenaFlow.cartFlow();
+  });
+  
+  it("deve verficar o fluxo de adicionar itens ao carrinho e de modificar suas quantidades", async () => {
     await myllenaFlow.alternativeCartFlow();
   });
 });
